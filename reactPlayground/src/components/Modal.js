572d1cb5afff39be70ee686148ceeafe9f6898d2
mickykhd <mickykhd07@gmail.com>
2022-11-23 04:58:58 +0530

    updated

--- reactPlayground/src/components/Modal.js
+++ reactPlayground/src/components/Modal.js
@@ -1,41 +1,10 @@
 import React from "react";
 
-import { useState, useEffect, useRef } from "react";
+import { useRef } from "react";
 import "./Modal.css";
 function Modal() {
   const gg = useRef(null);
-  const kk = useRef(null);
-  const pp = useRef(null);
 
-  // useEffect(() => {
-  //   const vv = kk.current;
-  //   const bb = gg.current;
-  //   try {
-  //     vv.addEventListener("click", () => {
-  //       bb.showModal();
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-
-  //   return () => {
-  //     vv.removeEventListener("click", () => {
-  //       bb.showModal();
-  //     });
-  //   };
-  // }, []);
-  // useEffect(() => {
-  //   const vv = kk.current;
-  //   const bb = gg.current;
-  //   pp.current.addEventListener("click", () => {
-  //     bb.close();
-  //   });
-  //   return () => {
-  //     bb.removeEventListener("click", () => {
-  //       gg.current.close();
-  //     });
-  //   };
-  // }, []);
   const handleSubmit = () => {
     gg.current.showModal();
   };
@@ -48,12 +17,12 @@ function Modal() {
       <div className="modal-cntnr">
         <dialog ref={gg} className="modal">
           <h1>hello</h1>
-          <button ref={pp} onClick={handleClose} className="close-modal">
+          <button onClick={handleClose} className="close-modal">
             close modal
           </button>
         </dialog>
 
-        <button ref={kk} className="btn" onClick={handleSubmit}>
+        <button className="btn" onClick={handleSubmit}>
           close
         </button>
       </div>
